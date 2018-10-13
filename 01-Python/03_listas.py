d993679da744d11dd0d312036b79a08a21baec99
alexm91 <alex.moralesc91@gmail.com>
2018-10-13 09:38:52 -0500

    listas fin

--- 01-Python/03_listas.py
+++ 01-Python/03_listas.py
@@ -20,3 +20,17 @@ print(arregloNumeros[-1])
 print(arregloNumeros[-2])
 print(arregloNumeros[3:-2])
 print(arregloNumeros[-5:-2])
+
+print(7 in arregloNumeros)
+print(15 in arregloNumeros)
+print(len(arregloNumeros))
+
+arregloNumeros.append(10)
+print(arregloNumeros)
+arregloNumeros.pop(-3)
+print(arregloNumeros)
+arregloNumeros.insert(1, 1.2)
+print(arregloNumeros)
+
+del arregloNumeros[1:4]
+print(arregloNumeros)
