ed41afbcf8922a906d19b44ca185f1c3c9ac0dbb
alexm91 <alex.moralesc91@gmail.com>
2018-10-13 09:29:48 -0500

    listas

---
+++ 01-Python/03_listas.py
@@ -0,0 +1,22 @@
+arreglo = []
+
+arregloCosas = [
+    "a",
+    1,
+    True,
+    False,
+    None,
+    10.1,
+    [1, 2, 3]
+]
+
+arregloNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0,3[
+print(arregloNumeros[0:5])
+print(arregloNumeros[3:5])
+print(arregloNumeros[:2])
+print(arregloNumeros[2:])
+print(arregloNumeros[-1])
+print(arregloNumeros[-2])
+print(arregloNumeros[3:-2])
+print(arregloNumeros[-5:-2])
